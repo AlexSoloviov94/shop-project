5e7165ae1cc1e951465d392c35c9a93e454e2108
AlexSoloviov94 <sanyas9435@gmail.com>
2023-01-15 21:18:52 +0200

    slider

--- app.js
+++ app.js
@@ -5,25 +5,110 @@ let addToCartBtns = document.querySelectorAll(".add-btn");
 // console.log(productsCountEl);
 // console.log(addToCartBtns);
 
+// for (let i = 0; i < addToCartBtns.length; i++) {
+//   addToCartBtns[i].addEventListener("click", function () {
+//     productsCountEl.textContent = +productsCountEl.textContent + 1;
+//   });
+// }
+
+// let wishBtn = document.querySelectorAll(".wish-list");
+// let wishImg = document.querySelectorAll(".wish-img");
+
+// for (let i = 0; i < wishBtn.length; i++) {
+//   wishBtn[i].addEventListener("click", function () {
+//     let src = wishImg[i].getAttribute("src");
+
+//     if (src === "images/icon 5.png") {
+//       wishImg[i].setAttribute("src", "images/icon 7.png");
+//       wishBtn[i].classList.add("wish-list-liked");
+//     } else {
+//       wishImg[i].setAttribute("src", "images/icon 5.png");
+//       wishBtn[i].classList.remove("wish-list-liked");
+//     }
+//   });
+// }
+
 for (let i = 0; i < addToCartBtns.length; i++) {
-  addToCartBtns[i].addEventListener("click", function () {
-    productsCountEl.textContent = +productsCountEl.textContent + 1;
-  });
+    addToCartBtns[i].addEventListener("click", function () {
+        productsCountEl.textContent = +productsCountEl.textContent + 1;
+    });
+}
+// change like state
+
+let likeBtns = document.querySelectorAll(".wish-list");
+console.log(likeBtns);
+
+likeBtns.forEach((btn) =>
+    btn.addEventListener("click", function (e) {
+        e.target.classList.toggle("liked");
+        e.target.style.transition = "all 3s ease-out";
+    })
+);
+
+let moreDetailsBtns = document.querySelectorAll(".more-btn");
+let modal = document.querySelector(".modal");
+// let btnClose = document.querySelector(".btn-close");
+
+// moreDetailsBtns.forEach((item) => {
+//     item.addEventListener("click", openModal);
+// });
+
+// btnClose.addEventListener("click", closeModal);
+
+// function openModal() {
+//     modal.classList.add("show");
+//     modal.classList.remove("hide");
+// }
+
+// function closeModal() {
+//     modal.classList.add("hide");
+//     modal.classList.remove("show");
+// }
+
+// modal.addEventListener("click", function (e) {
+//     if (e.target === modal) {
+//         closeModal();
+//     }
+// });
+
+let closeBtn = document.querySelector(".btn-close");
+
+moreDetailsBtns.forEach((item) => item.addEventListener("click", openModal));
+closeBtn.addEventListener("click", closeModal);
+
+function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
 }
 
-let wishBtn = document.querySelectorAll(".wish-list");
-let wishImg = document.querySelectorAll(".wish-img");
+function closeModal() {
+    modal.classList.remove("show");
+    modal.classList.add("hide");
+}
 
-for (let i = 0; i < wishBtn.length; i++) {
-  wishBtn[i].addEventListener("click", function () {
-    let src = wishImg[i].getAttribute("src");
-
-    if (src === "images/icon 5.png") {
-      wishImg[i].setAttribute("src", "images/icon 7.png");
-      wishBtn[i].classList.add("wish-list-liked");
-    } else {
-      wishImg[i].setAttribute("src", "images/icon 5.png");
-      wishBtn[i].classList.remove("wish-list-liked");
+modal.addEventListener("click", function (e) {
+    if (e.target === modal) {
+        closeModal();
     }
-  });
+});
+
+document.querySelector(".submit-form").addEventListener("click", function (e) {
+    e.preventDefault();
+});
+
+// slider
+
+$(".slider-block").slick({
+    dots: true,
+});
+
+window.addEventListener("scroll", openModalScroll);
+
+function openModalScroll() {
+    /*let scrollClientHeight;*/
+    if (window.pageYOffset > document.body.scrollHeight / 2) {
+        modal.classList.add("show");
+        modal.classList.remove("hide");
+        window.removeEventListener("scroll", openModalScroll);
+    }
 }
